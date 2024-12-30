009d91e72efe044f247a8415f69226c60f1fa21e
Juliana Arenas <juliana.arenas@onecontact.com.co>
2024-12-30 13:54:27 -0500

    actualización de estilos :)

--- cronometro-react/src/Components/Cronometro.jsx
+++ cronometro-react/src/Components/Cronometro.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react"
 import "./styles.css"
+
 const Chronometer = () => {
         const [time, setTime] = useState(0)
         const [isRunning, setisRunning] = useState(false)
